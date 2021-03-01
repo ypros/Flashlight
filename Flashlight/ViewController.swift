a5238fe4ebfcbe39b1c80224df728cee3e8c8915
Yuriy Prosvirnin <prosvirniny@gmail.com>
2021-03-02 00:10:43 +0300

    Added torch ON | OFF

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -23,15 +24,39 @@ class ViewController: UIViewController {
     
     /// Changes view background color
     fileprivate func updateUI() {
-        if currectColor >= colors.count {
+        if currectColor >= colors.endIndex {
             currectColor = 0
         }
         view.backgroundColor = colors[currectColor]
+        
+        toggleTorch(currectColor)
     }
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         currectColor += 1
         updateUI()
+    }
+    
+    
+    /// Turns torch On or Off by the light level
+    /// - Parameter level: the brightness level of the torch
+    func toggleTorch(_ level: Int) {
+        guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+        guard device.hasTorch else { print("Torch isn't available"); return }
+
+        let on: Bool = (level != colors.endIndex - 1)
+        let maxLevel = min(AVCaptureDevice.maxAvailableTorchLevel.significand, 1.0)
+        let step = Float(level)/Float(colors.count - 1) * maxLevel
+        
+        do {
+            try device.lockForConfiguration()
+            device.torchMode = on ? .on : .off
+            
+            if on { try device.setTorchModeOn(level: maxLevel - step) }
+            device.unlockForConfiguration()
+        } catch {
+            print("Torch can't be used")
+        }
     }
     
 }
