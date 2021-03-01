f0eb11835732a1a0cfc3113717061e8594ee251d
Yuriy Prosvirnin <prosvirniny@gmail.com>
2021-03-01 18:09:38 +0300

    Initial Commit

---
+++ Flashlight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashlight
+//
+//  Created by YURY PROSVIRNIN on 01.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
